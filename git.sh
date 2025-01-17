65d36d3a6a6d963955652e8d58df19ef37d0a5da
andrecfoss <andrecfoss@outlook.com>
2025-01-17 22:51:49 +0000

    refactor script

--- git.sh
+++ git.sh
@@ -1,39 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
-# Check if Git is installed
-if ! command -v git &> /dev/null; then
-  sudo apt update && sudo apt install git -y
-else
-  echo "Git is already installed!"
-fi
+# Function to check if Git is installed
+check_git() {
+  if ! command -v git &> /dev/null; then
+    sudo apt update && sudo apt install git -y
+  else
+    echo "Git is already installed!"
+  fi
+}
 
-echo -e $(git --version) "\n"
+# Function to check GitHub credentials
+check_github_credentials() {
+  if [ -z "$(git config --global user.name)" ] || [ -z "$(git config --global user.email)" ]; then
+    read -r -p "Enter GitHub username: " username
+    read -r -p "Enter GitHub e-mail address: " email
+    git config --global user.name "$username"
+    git config --global user.email "$email"
+    echo "GitHub credentials set successfully!"
+  else
+    echo "GitHub credentials already set!"
+  fi
+}
+
+# Function to check SSH key
+check_ssh_key() {
+  if [ ! -f ~/.ssh/id_ed25519 ]; then
+    if [ -z "$email" ]; then
+      read -r -p "Enter your email for SSH key: " email
+    fi
+    ssh-keygen -t ed25519 -C "$email"
+  else
+    echo "SSH key already exists!"
+  fi
+}
+
+# Main script execution
+check_git
+echo -e "$(git --version)\n"
 sleep 1
 
-# Check GitHub Credentials
-if [ -z "$(git config --global user.name)" ] || [ -z "$(git config --global user.email)" ]; then
-  read -p "Enter GitHub username: " username
-  read -p "Enter GitHub e-mail address: " email
-  git config --global user.name "$username"
-  git config --global user-email "$email"
-  echo "GitHub credentials set successfully!"
-else
-  echo "GitHub credentials already set!"
-fi
-
-echo -e $(git config --list) "\n"
+check_github_credentials
+echo -e "$(git config --list)"
 sleep 1
 
-# Check SSH Key
-if [ ! -f ~/.ssh/id_ed25519 ]; then
-  ssh-keygen -t ed25519 -C "$email"
-else
-  echo "SSH Key already generated previously!"
-  echo -e "To check the key: cat ~/.ssh/id_ed25519.pub\n"
-fi
-
-sleep 1
+check_ssh_key
 
 if [ -f ~/.ssh/id_ed25519.pub ]; then
   if ! grep -q "$(cat ~/.ssh/id_ed25519.pub)" ~/.ssh/authorized_keys; then
